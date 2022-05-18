e7ff25157abec64338f8f10d4f5a200ca2b93e6b
Germano Kuerten <germanokuerten@gmail.com>
2022-05-18 16:34:51 -0700

    css update

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -6,7 +6,12 @@ function Background (props) {
             <div className="background">
             <div class="stars"></div>
             <div class="twinkling"></div>
+            <div class="clouds3"></div>
             <div class="clouds"></div>
+            <div class="clouds2"></div>
+
+            {/* <div class="laserbeams"></div> */}
+            
             </div>
         </>
     )
